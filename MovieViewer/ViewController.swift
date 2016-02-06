3c84d713f370b7bc3f5890699f9adbd25a6eeafd
Zean Tsoi <zeantsoi@gmail.com>
2016-02-05 21:20:48 -0800

    Initial Commit

---
+++ MovieViewer/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  MovieViewer
+//
+//  Created by Zean Tsoi on 2/5/16.
+//  Copyright © 2016 Zean Tsoi. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
